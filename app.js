f5c72cb60a28f72864d260e7408846e73e1a6828
Andrew Atkins <S551050@nwmissouri.edu>
2026-04-08 13:39:04 -0500

    Part 3

--- app.js
+++ app.js
@@ -1,58 +1,63 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const connectionString = process.env.MONGO_CON;
-
-mongoose.connect(connectionString);
-
-// Get the default connection
-var db = mongoose.connection;
-
-// Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-  console.log("Connection to DB succeeded");
-});
-
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
+
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+  console.log("Connection to DB succeeded");
+  let reseed = true;
+  if (reseed) { recreateDB(); }
+});
+
+var Costume = require("./models/costume");
+
+async function recreateDB(){
+    try {
+        await Costume.deleteMany();
+        let instance1 = new Costume({costume_type:"ghost", size:'large', cost:15.4});
+        instance1.save().then(doc => {
+            console.log("First object saved");
+        }).catch(err => {
+            console.error(err);
+        });
+    } catch (err) {
+        console.error("Seed error:", err);
+    }
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var unicyclesRouter = require('./routes/unicycles'); // Added for Part 5
-var gridRouter = require('./routes/grid');           // Added for Part 7
-var pickRouter = require('./routes/pick');           // Added for Part 8
+
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use('/unicycles', unicyclesRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/gridbuild', gridRouter);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/selector', pickRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
