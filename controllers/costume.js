acc308adca2f0eb4840ed72b34cb30869f488d8f
Andrew Atkins <S551050@nwmissouri.edu>
2026-04-09 21:19:50 -0500

    Part 2

--- controllers/costume.js
+++ controllers/costume.js
@@ -21,6 +21,7 @@ exports.costume_detail = async function(req, res) {
     }
 };
 
+
 exports.costume_create_post = async function(req, res) {
     console.log("Body received:", req.body); 
     let document = new Costume();
@@ -48,10 +49,22 @@ exports.costume_delete = async function(req, res) {
     }
 };
 
-exports.costume_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+exports.costume_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Costume.findById(req.params.id)
+        // Do updates of properties
+        if(req.body.costume_type) toUpdate.costume_type = req.body.costume_type;
+        if(req.body.cost) toUpdate.cost = req.body.cost;
+        if(req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
-
 
 exports.costume_view_all_Page = async function(req, res) {
     try {
